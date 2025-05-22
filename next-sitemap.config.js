d210a724638399c96112f3b37c666189e41a5c95
unknown <berkucar0901@gmail.com>
2025-05-22 16:03:32 +0300

    .

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.drmehmetilteberbahadir.com", // kendi domaininle değiştir
+  generateRobotsTxt: true, // robots.txt de oluşturur
+  changefreq: "weekly",
+  priority: 0.7,
+};
